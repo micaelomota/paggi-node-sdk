daa81539752b29348fda1fc256b82288a5f3ff20
Micael Mota <micaelomota@hotmail.com>
2018-08-19 01:55:34 -0300

    setting a client

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var Buffer = require('safe-buffer').Buffer
+var Buffer = require('safe-buffer').Buffer;
+var Client = require('./lib/client');
+var paggi = require('./lib/paggi');
 
 module.exports = function (_username, _password, _production) {
   var production = _production != undefined ? _production : false;
   var basicAuth = 'Basic ' + new Buffer(_username + ':' + _password).toString('base64');
+  var apiClient = Client(basicAuth, paggi.getAPI(production));
 
   return {
-    charges: require('./lib/charges')(basicAuth, production),
-    cards: require('./lib/cards')(basicAuth, production),
-    customers: require('./lib/customers')(basicAuth, production),  
-    banks: require('./lib/banks')(basicAuth, production)
+    charges: require('./lib/charges')(apiClient),
+    cards: require('./lib/cards')(apiClient),
+    customers: require('./lib/customers')(apiClient),  
+    banks: require('./lib/banks')(apiClient)
   };
 }
