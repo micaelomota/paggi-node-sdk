af2405c447b63976d0f8710a3e23aa098074a316
Micael Mota <micaelomota@hotmail.com>
2018-08-19 02:25:34 -0300

    using eslint to prettify my code :D

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var Buffer = require('safe-buffer').Buffer;
-var Client = require('./lib/client');
-var paggi = require('./lib/paggi');
+var Buffer = require('safe-buffer').Buffer
+var Client = require('./lib/client')
+var paggi = require('./lib/paggi')
 
 module.exports = function (_username, _password, _production) {
-  var production = _production != undefined ? _production : false;
-  var basicAuth = 'Basic ' + new Buffer(_username + ':' + _password).toString('base64');
-  var apiClient = Client(basicAuth, paggi.getAPI(production));
+  var production = _production !== undefined ? _production : false
+  var basicAuth = 'Basic ' + Buffer.from(_username + ':' + _password).toString('base64')
+  var apiClient = Client(basicAuth, paggi.getAPI(production))
 
   return {
     charges: require('./lib/charges')(apiClient),
     cards: require('./lib/cards')(apiClient),
-    customers: require('./lib/customers')(apiClient),  
+    customers: require('./lib/customers')(apiClient),
     banks: require('./lib/banks')(apiClient)
-  };
+  }
 }
